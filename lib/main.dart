192288403d3922783ab1217c9d4f46a0ba8e4761
Ayevbeosa Iyamu <ayevbeosa.j@gmail.com>
2023-05-23 23:48:40 +0100

    init dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:casino_test/src/presentation/ui/character_screen.dart';
+import 'package:casino_test/features/characters/presentation/bloc/character_bloc.dart';
+import 'package:casino_test/features/characters/presentation/views/characters_screen.dart';
+import 'package:casino_test/injection_container.dart' as di;
+import 'package:casino_test/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
-
-import 'injection_container.dart' as di;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = const SimpleBlocObserver();
   di.init();
   runApp(const MyApp());
 }
@@ -15,7 +18,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test app',
-      home: CharactersScreen(),
+      home: BlocProvider(
+        create: (_) => di.getIt<CharacterBloc>()..add(const GetCharacters()),
+        child: const CharactersScreen(),
+      ),
     );
   }
 }
